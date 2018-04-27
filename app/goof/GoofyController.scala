7962f50a23962258c94b8fd33727f8d8aaaf3ec0
Joseph Gerardot <indyJAG@gmail.com>
2018-04-27 15:27:59 -0400

    Complete update to GoofyAPI. It does throw an exception for certain invalid inputs, and a production version would certainly check for these but I don't think input validation is a very interesting way to spend my time on this project.

--- app/goof/GoofyController.scala
+++ app/goof/GoofyController.scala
@@ -14,27 +14,27 @@ class GoofyController @Inject()(repo: GoofyRepo) extends Controller {
         def writes(num: Int): JsValue = {
             Json.obj("num" -> num)
         }
-    } // now we can write our ints out as Json in a field of a name we chose
+    } 
 
     // I'll implement these in terms of Futures because that seems to be the way people prefer, although Play claims to be non-blocking always
     // I'm not implementing my own ActionBuilder, though, because I don't think I need any of the things gained from it
-    def list = Action.async { implicit request =>
+    def list(id: Int) = Action.async { implicit request =>
         logger.trace("List: ") // log that we received a request for the list
-        repo.list().map(set => Ok(Json.toJson(set.toSeq))) // Make the list into a Sequence and then into a JsArray, sending back the the Ok result
+        repo.list(id).map(set => Ok(Json.toJson(set.toSeq))) // Make the list into a Sequence and then into a JsArray, sending back the the Ok result
     }
     
-    def add(num: Int) = Action.async { implicit request =>
+    def add(id: Int, num: Int) = Action.async { implicit request =>
         logger.trace(s"Adding $num")
-        val sometimeNum = repo.add(num)
+        val sometimeNum = repo.add(id,num)
         sometimeNum.map( _ match {
             case Some(num) => Ok(Json.toJson(num))
             case _ => NotFound(Json.toJson("Already in"))
         })
     }
     // This may seem like duplicate code to the above, but is really is only so for now, so it doesn't make sense to merge them. 
-    def get(num: Int) = Action.async { implicit request =>
+    def get(id: Int, num: Int) = Action.async { implicit request =>
         logger.trace(s"Getting $num")
-        val sometimeNum = repo.get(num)
+        val sometimeNum = repo.get(id, num)
         sometimeNum.map( _ match {
             case Some(num) => Ok(Json.toJson(num));
             case _ => NotFound(Json.toJson("Wasn't Found"));
--- app/goof/GoofyRepo.scala
+++ app/goof/GoofyRepo.scala
@@ -5,7 +5,8 @@ import javax.inject.{Inject, Singleton}
 import akka.actor.ActorSystem
 import play.api.libs.concurrent.CustomExecutionContext
 import scala.collection.mutable.Set // Everyone likes immutable stuff but I actually want to modify this one
-import scala.colection.mutable.HashMap
+import scala.collection.mutable.BitSet
+import scala.collection.mutable.HashMap
 import scala.concurrent.{ExecutionContext, Future}
 
 class GoofyExecutionContext @Inject()(actorSystem: ActorSystem) extends CustomExecutionContext(actorSystem, "repository.dispatcher")
@@ -22,13 +23,12 @@ class GoofyRepoImpl @Inject()(implicit ec: GoofyExecutionContext) extends GoofyR
     // I can't figure out how the example adds new posts even though all the types are immutable, so I'm using a mutable
     // This may cause problems with the highly asynchronous nature of Play (eg: race conditions)
     // Gaffs is now a HashMap of Sets. I didn't choose a Set of Sets because then I wouldn't have an index to a particular value.
-    private val gaffs: HashMap[Set[Int]] = new HashMap(); // This time all the values will come from POSTs
+    private val gaffs: HashMap[Int,Set[Int]] = new HashMap(); // This time all the values will come from POSTs
 
-    override def list(id: int): Future[Iterable[Int]] = {
+    override def list(id: Int): Future[Iterable[Int]] = {
         Future { gaffs(id) } // now list retrieves the Set associated with the key
     }
 
-    // I was going to use fancy pattern-matching for this, but research suggested if/else was better if you don't already have Options
     override def add(id: Int, num: Int): Future[Option[Int]] =  {
         Future {
             if(gaffs.contains(id)) { // check if the set they want already exists
@@ -37,11 +37,11 @@ class GoofyRepoImpl @Inject()(implicit ec: GoofyExecutionContext) extends GoofyR
                     None // if the number is already in, return None
                 } else {
                     set += num // add the num in
-                        Some(num) // return it, signaling it was added
+                    Some(num) // return it, signaling it was added
                 }
             } else { // if the Set isn't in there already
-                gaffs += (id -> new Set(num)) // add the id as a key pointing to a new set
-                    Some(num)
+                gaffs += (id -> new BitSet(num)) // add the id as a key pointing to a new set
+                Some(num)
             }
         }
     }
